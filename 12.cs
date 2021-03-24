ddc99c000a829e15e7f7ebf2a2d6a9c328dadfa8
JohnMoreta <78990609+JohnMoreta@users.noreply.github.com>
2021-03-23 22:50:06 -0400

    Add files via upload

---
+++ 12.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Empresa
+{
+    class Empresa
+    {
+         string[] nombres;
+         float[] sueldo;
+
+        public void Cargar()
+        {
+            nombres = new string[5];
+            sueldo = new float[5];
+            for (int f = 0; f < nombres.Length; f++)
+            {
+                Console.Write("Ingrese el nombre del empleado:");
+                nombres[f] = Console.ReadLine();
+                Console.Write("Ingrese el sueldo:");
+                string linea;
+                linea = Console.ReadLine();
+                sueldo[f] = float.Parse(linea);
+            }
+        }
+
+        public void MayorSueldo()
+        {
+            float mayor;
+            int pos;
+            mayor = sueldo[0];
+            pos = 0;
+            for (int f = 1; f < nombres.Length; f++)
+            {
+                if (sueldo[f] > mayor)
+                {
+                    mayor = sueldo[f];
+                    pos = f;
+                }
+            }
+            Console.WriteLine("El empleado con sueldo mayor es " + nombres[pos]);
+            Console.WriteLine("Tiene un sueldo:" + mayor);
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            Empresa calulo = new Empresa();
+            calulo.Cargar();
+            calulo.MayorSueldo();
+        }
+    }
+}
